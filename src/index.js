dd8e7c1446821d25069fb0afc3e6e44bbd7b48cc
Syed Muhammed Umair <umairrsyedd.study@gmail.com>
2020-03-04 23:58:14 +0530

    Project Incomplete But Basics Understood

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: null
-		};
-	}
+	// 	constructor(props) {
+	// 		super(props);
+	// 		this.state = {
+	// 			value: null
+	// 		};
+	// 	}
 	render() {
 		return (
 			<button
 				className="square"
 				onClick={() => {
-					this.setState({ value: "X" });
+					this.props.onClick();
 				}}
 			>
-				{this.state.value}
+				{this.props.value}
 			</button>
 		);
 	}
@@ -31,7 +31,12 @@ class Board extends React.Component {
 		};
 	}
 	renderSquare(i) {
-		return <Square value={i} />;
+		return (
+			<Square
+				value={this.state.squares[i]}
+				onClick={() => this.handleClick(i)}
+			/>
+		);
 	}
 
 	render() {
